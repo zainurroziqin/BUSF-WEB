e28257455adf60a7f7553c3515f57b178bed5077
zainurroziqin <zainur7jan2002@gmail.com>
2021-12-07 01:44:47 +0700

    menambah alert dan mengatasi gagal edit data

--- websites/EkandangB.php
+++ websites/EkandangB.php
@@ -8,25 +8,43 @@
 $id = $_GET["id"];
 
 // query data kandang A berdasarkan id
-$kdb = query("SELECT * FROM kandang_b WHERE id = $id")[0];
+$kda = query("SELECT * FROM kandang_b WHERE id = $id")[0];
 
 // cek apakah tombol submit sudah ditekan atau belum
 if ( isset($_POST["submit"])){
 
 // cek apakah data berhasil ditambahkan atau tidak
-if (ubahA ($_POST) > 0 ) {
+if (ubahb ($_POST) > 0 ) {
     // menggunakan javascript
     echo "
-    <script>
-    alert ('data berhasil diubah!')
-    document.location.href ='KandangB.php';
-    </script>";
+        <script>
+            setTimeout(function() { 
+                Swal.fire({
+                    title: 'Berhasil!',
+                    text: 'Data Berhasil Diedit',
+                    icon: 'success',
+                });
+            },10);
+            window.setTimeout(function(){ 
+                window.location.replace('KandangB');
+            },1800);
+        </script>
+    ";
 } else {
     echo "
-    <script>
-    alert ('data gagal diubah!')
-    document.location.href ='KandangB.php';
-    </script>";
+        <script>
+            setTimeout(function() { 
+                Swal.fire({
+                    title: 'Gagal!',
+                    text: 'Data Gagal Diedit',
+                    icon: 'gagal',
+                });
+            },10);
+            window.setTimeout(function(){ 
+                window.location.replace('KandangB');
+            },2500);
+        </script>
+    ";
 }
 
 
@@ -46,7 +64,7 @@ $tanggal = date("Ymd");
                             </div>
                             <ul class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="Dashboard.php"><i class="feather icon-home"></i></a></li>
-                                <li class="breadcrumb-item"><a href="KandangB.php">Kandang B</a></li>
+                                <li class="breadcrumb-item"><a href="KandangA.php">Kandang B</a></li>
                                 <!-- <li class="breadcrumb-item"><a href="#!">Basic Initialization</a></li> -->
                             </ul>
                         </div>
@@ -68,7 +86,7 @@ $tanggal = date("Ymd");
 
                             <div class="mx-auto">
                                 <div class="mb-3">
-                                    <input type="hidden" name="id" value="<?= $kdb["id"];?>">
+                                    <input type="hidden" name="id" value="<?= $kda["id"];?>">
 
                                   
                                 <input type="hidden" class="form-control form-control-user"  placeholder="Masukkan Nama..." name="tanggal" required  value="<?php echo date("Y-m-d"); ?>">
@@ -86,32 +104,32 @@ $tanggal = date("Ymd");
                                 <div class="mb-3">
 
                                     <label for="pakan_total" class="form-label">Total Pakan</label>
-                                    <input type="text" class="form-control" id="rupiah" aria-describedby="emailHelp" placeholder="Total pakan (kg).." name="pakan_total" value="<?= $kdb["pakan_total"]; ?>" required="">
+                                    <input type="text" class="form-control" id="rupiah" aria-describedby="emailHelp" placeholder="Total pakan (kg).." name="pakan_total" value="<?= $kda["pakan_total"]; ?>" required="">
 
                                 </div>
                                 <div class="mb-3">
                                     <label for="sisa_1" class="form-label">Sisa Pakan Baris 1</label>
-                                    <input type="number" class="form-control" id="sisa_1" placeholder="Sisa pakan baris 1" name="sisa_1" value="<?= $kdb["sisa_1"]; ?>" required="">
+                                    <input type="number" class="form-control" id="sisa_1" placeholder="Sisa pakan baris 1" name="sisa_1" value="<?= $kda["sisa_1"]; ?>" required="">
                                 </div>
                                 <div class="mb-3">
                                     <label for="sisa_2" class="form-label">Sisa Pakan Baris 2</label>
-                                    <input type="number" class="form-control" id="sisa_2" placeholder="Sisa pakan baris 2" name="sisa_2" value="<?= $kdb["sisa_2"]; ?>" required="">
+                                    <input type="number" class="form-control" id="sisa_2" placeholder="Sisa pakan baris 2" name="sisa_2" value="<?= $kda["sisa_2"]; ?>" required="">
                                 </div>
                                 <div class="mb-3">
                                     <label for="sisa_3" class="form-label">Sisa Pakan Baris 3</label>
-                                    <input type="number" class="form-control" id="sisa_3" placeholder="Sisa pakan baris 3" name="sisa_3" value="<?= $kdb["sisa_3"]; ?>" required="">
+                                    <input type="number" class="form-control" id="sisa_3" placeholder="Sisa pakan baris 3" name="sisa_3" value="<?= $kda["sisa_3"]; ?>" required="">
                                 </div>
                                 <div class="mb-3">
                                     <label for="sisa_4" class="form-label">Sisa Pakan Baris 4</label>
-                                    <input type="number" class="form-control" id="sisa_4" placeholder="Sisa pakan baris 4" name="sisa_4" value="<?= $kdb["sisa_4"]; ?>" required="">
+                                    <input type="number" class="form-control" id="sisa_4" placeholder="Sisa pakan baris 4" name="sisa_4" value="<?= $kda["sisa_4"]; ?>" required="">
                                 </div>
                                 <div class="mb-3">
                                     <label for="sisa_5" class="form-label">Sisa Pakan Baris 5</label>
-                                    <input type="number" class="form-control" id="sisa_5" placeholder="Sisa pakan baris 5" name="sisa_5" value="<?= $kdb["sisa_5"]; ?>" required="">
+                                    <input type="number" class="form-control" id="sisa_5" placeholder="Sisa pakan baris 5" name="sisa_5" value="<?= $kda["sisa_5"]; ?>" required="">
                                 </div>
                                 <div class="mb-3">
                                     <label for="sisa_6" class="form-label">Sisa Pakan Baris 6</label>
-                                    <input type="number" class="form-control" id="sisa_6" placeholder="Sisa pakan baris 6" name="sisa_6" value="<?= $kdb["sisa_6"]; ?>" required="">
+                                    <input type="number" class="form-control" id="sisa_6" placeholder="Sisa pakan baris 6" name="sisa_6" value="<?= $kda["sisa_6"]; ?>" required="">
                                 </div>
                             </div>
 
@@ -119,11 +137,11 @@ $tanggal = date("Ymd");
                                 <h3>Telur <i class="fas fa-egg"></i></h3>
                                 <div class="mb-3">
                                     <label for="jumlah_telur" class="form-label">Jumlah Telur</label>
-                                    <input type="number" class="form-control" id="jumlah_telur" placeholder="Jumlah telur" name="jumlah_telur" value="<?= $kdb["jumlah_telur"]; ?>" required="">
+                                    <input type="number" class="form-control" id="jumlah_telur" placeholder="Jumlah telur" name="jumlah_telur" value="<?= $kda["jumlah_telur"]; ?>" required="">
                                 </div>
                                 <div class="mb-3">
                                     <label for="berat_telur" class="form-label">Berat Telur</label>
-                                    <input type="text" class="form-control" id="berat_telur" placeholder="Berat telur (kg)" name="berat_telur" value="<?= $kdb["berat_telur"]; ?>" required="">
+                                    <input type="text" class="form-control" id="berat_telur" placeholder="Berat telur (kg)" name="berat_telur" value="<?= $kda["berat_telur"]; ?>" required="">
                                 </div>
 
 
@@ -133,15 +151,15 @@ $tanggal = date("Ymd");
                                 <h3>Suhu <i class="fas fa-temperature-high"></i></h3>
                                 <div class="mb-3">
                                     <label for="suhu_pagi" class="form-label">Suhu Pagi</label>
-                                    <input type="text" class="form-control" id="suhu_pagi" placeholder="Suhu pagi hari" name="suhu_pagi" value="<?= $kdb["suhu_pagi"]; ?>" required="" maxlength="5">
+                                    <input type="text" class="form-control" id="suhu_pagi" placeholder="Suhu pagi hari" name="suhu_pagi" value="<?= $kda["suhu_pagi"]; ?>" required="" maxlength="5">
                                 </div>
                                 <div class="mb-3">
                                     <label for="suhu_siang" class="form-label">Suhu Siang</label>
-                                    <input type="text" class="form-control" id="suhu_siang" placeholder="Suhu siang hari" name="suhu_siang" value="<?= $kdb["suhu_siang"]; ?>" required="" maxlength="5">
+                                    <input type="text" class="form-control" id="suhu_siang" placeholder="Suhu siang hari" name="suhu_siang" value="<?= $kda["suhu_siang"]; ?>" required="" maxlength="5">
                                 </div>
                                 <div class="mb-3">
                                     <label for="suhu_sore" class="form-label">Suhu Sore</label>
-                                    <input type="text" class="form-control" id="suhu_sore" placeholder="Suhu sore hari" name="suhu_sore" value="<?= $kdb["suhu_sore"]; ?>" required="" maxlength="5">
+                                    <input type="text" class="form-control" id="suhu_sore" placeholder="Suhu sore hari" name="suhu_sore" value="<?= $kda["suhu_sore"]; ?>" required="" maxlength="5">
 
 
                                 </div>
@@ -156,23 +174,23 @@ $tanggal = date("Ymd");
                                 <h3>Ayam <i class="fas fa-kiwi-bird"></i></h3>
                                 <div class="mb-3">
                                     <label for="mati" class="form-label">Jumlah Ayam Mati</label>
-                                    <input type="number" class="form-control" id="mati" placeholder="Jumlah ayam mati" name="mati" value="<?= $kdb["mati"]; ?>" required="" min="0">
+                                    <input type="number" class="form-control" id="mati" placeholder="Jumlah ayam mati" name="mati" value="<?= $kda["mati"]; ?>" required="" min="0">
                                 </div>
                                 <div class="mb-3">
                                     <label for="mati" class="form-label">Jumlah Ayam Afkir</label>
-                                    <input type="number" class="form-control" id="mati" placeholder="Jumlah ayam afkir" name="afkir" value="<?= $kdb["mati"]; ?>" required="" min="0">
+                                    <input type="number" class="form-control" id="mati" placeholder="Jumlah ayam afkir" name="afkir" value="<?= $kda["mati"]; ?>" required="" min="0">
                                 </div>
                                 <br><br><br><br><br><br><br><br>
                                 <div class="mb-3">
                                     <label for="" class="form-label">Diisi Oleh</label>
-                                    <input type="text" class="form-control" id="" placeholder="Masukkan suhu sore hari" name="nama" value="<?= $kdb["nama"]?>" readonly="">
+                                    <input type="text" class="form-control"  name="nama" value="<?= $kda["nama"]?>" readonly="">
                                     <br>
 
                                     <div class="mb-3">
                               <div class="form-group">
                                     <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref" name="keterangan"  required>
-                                        <option value="Terkonfirmasi" <?php if ($kdb['keterangan'] == 'Terkonfirmasi') {echo "selected";} ?>>Terkonfirmasi</option>
-                                        <option value="Belum Dikonfirmasi" <?php if ($kdb['keterangan'] == 'Belum Dikonfirmasi') {echo "selected";} ?>>Belum Dikonfirmasi</option>
+                                        <option value="Terkonfirmasi" <?php if ($kda['keterangan'] == 'Terkonfirmasi') {echo "selected";} ?>>Terkonfirmasi</option>
+                                        <option value="Belum Dikonfirmasi" <?php if ($kda['keterangan'] == 'Belum Dikonfirmasi') {echo "selected";} ?>>Belum Dikonfirmasi</option>
 
                                     </select>
                                 </div>
