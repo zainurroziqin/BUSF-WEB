654dea57f3d4921f91c3b4d72715184a7bc722b3
Mohammad Rizky Ramadhani <80668526+ramadhani20@users.noreply.github.com>
2021-12-22 18:46:35 +0700

    Tambahan Cetak Excel

--- websites/RkandangA.php
+++ websites/RkandangA.php
@@ -32,23 +32,24 @@ $kandang_a= query("SELECT * FROM kandang_a")
 
             <div class="col-sm-12">
                 <div class="card">
-                    <div class="card-header">
-                        <!-- <h5>Kandang A</h5> --><center>
-                       <h3 >LAPORAN DATA KANDANG A</h3>
-                       <h3>BAROKAH UTAMA FARM</h3>
-                       </center>
-                    </div>
+                    
                     <div class="card-body">
                         <div class="dt-responsive table-responsive">
                         	 
-                            <table id="simpletable" class="table table-striped table-bordered nowrap">
+                            <table id="tbl_exporttable_to_xls" class="table table-striped table-bordered nowrap">
+
                                 <thead>
+                                    <tr>
+                            <td colspan = "21" align="center">    <h3 >LAPORAN DATA KANDANG A</h3></td>
+                      
+                       </tr>
+                       <tr>
+                       <td colspan ="21" align="center"><h3>BAROKAH UTAMA FARM</h3></td>
+                       </tr>
                                  <tr align="center">
-                                     <td rowspan="3">Hari</td>
-                                      <td rowspan="3">Minggu</td>
+                                     <td rowspan="3">Hari</td>                                     
                                        <td rowspan="3">Tanggal</td>
-                                        <td colspan="3">Jumlah ayam</td>
-                                      
+                                        <td colspan="3">Jumlah ayam</td>                                   
                                            <td colspan="8" >Pemberian Pakan</td>
                                             <td colspan="2">Produksi Telur</td>
                                       <td colspan="3">Suhu Kandang</td>
@@ -56,10 +57,7 @@ $kandang_a= query("SELECT * FROM kandang_a")
                                       <td rowspan="3">FCR</td>
                                       <td rowspan="3">Egg Mass</td>
                                  </tr>
-                                 <tr align="center">
-                                     
-                                      
-                                     
+                                 <tr align="center">                                                                                                          
                                         <td rowspan="2">Afkir</td>
                                          <td rowspan="2">Mati</td>
                                          <td rowspan="2">Jumlah Ayam</td>
@@ -89,11 +87,11 @@ $kandang_a= query("SELECT * FROM kandang_a")
                                 <tbody>
                                 
                                 <?php $i =1; ?>
-                                <?php $a =1; ?>
+                               
                           
                             <?php foreach ($kandang_a as $row ): ?>
                                 <td ><?=$i; ?></td>
-                         <td ><?=$a; ?></td>
+                        
                                 <td><?= date ('d F Y', strtotime ($row["tanggal"])); ?></td>
                                 <td><?= $row["afkir"]; ?> Ekor</td>
                                 <td><?= $row["mati"]; ?> Ekor</td>
@@ -117,22 +115,42 @@ $kandang_a= query("SELECT * FROM kandang_a")
                                 
                                 </tr>
                                 <?php $i ++; ?>
-                                  <?php $a ++; ?>
+                                
                             <?php endforeach; ?>
 
                                 </tbody>
                               
                             </table>
-                             
+                               
                         </div>
                     </div>
+                    <div class="mt-2 mb-2 ml-3 ">
+<button type="button"  class="btn btn-danger mr-4" onclick = "ExportToExcel('xlsx')"><i class="fas fa-edit" ></i> Cetak PDF</button>
+
+                         </div>
                 </div>
             </div>
         </div>
     </section>
 
 <!-- </div> -->
-
+<script src="https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js"></script>
+<script>
+   function ExportToExcel(type, fn, dl) {
+      var elt = document.getElementById('tbl_exporttable_to_xls');
+      var wb = XLSX.utils.table_to_book(elt, {
+         sheet: "sheet1",
+         dateNF: 'dd-mm-yyyy'
+      });
+      return dl ?
+      XLSX.write(wb, {
+         bookType: type,
+         bookSST: true,
+         type: 'base64'
+      }) :
+      XLSX.writeFile(wb, fn || ('Report Data Kandang A.' + (type || 'xlsx')));
+   }
+</script>
 <?php
     include 'komponen/closing-pages.php';
 ?>
