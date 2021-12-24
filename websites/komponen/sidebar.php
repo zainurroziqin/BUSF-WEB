74dbbe44112729783060e66557212831a732d0a8
Mohammad Rizky Ramadhani <80668526+ramadhani20@users.noreply.github.com>
2021-12-24 10:47:25 +0700

    Hapus Report

--- websites/komponen/sidebar.php
+++ websites/komponen/sidebar.php
@@ -56,7 +56,7 @@ if ( !isset($_SESSION["login"])){
                     <label>Report</label>
                 </li>
                 <li class="nav-item pcoded-hasmenu">
-                    <a href="#!" class="nav-link"><span class="pcoded-micon"><i class="fas fa-calendar-week"></i></span><span class="pcoded-mtext">Weekly Report</span></a>
+                    <a href="#!" class="nav-link"><span class="pcoded-micon"><i class="fas fa-calendar-week"></i></span><span class="pcoded-mtext">Laporan Kandang</span></a>
                     <ul class="pcoded-submenu">
                         <li><a href="RkandangA.php">Kandang A</a></li>
                         <li><a href="RkandangB.php">Kandang B</a></li>
@@ -64,15 +64,7 @@ if ( !isset($_SESSION["login"])){
                         <li><a href="RkandangD.php">kandang D</a></li>
                     </ul>
 				</li>
-                <li class="nav-item pcoded-hasmenu">
-                    <a href="#!" class="nav-link"><span class="pcoded-micon"><i class="fas fa-calendar-alt"></i></span><span class="pcoded-mtext">Monthly Report</span></a>
-                    <ul class="pcoded-submenu">
-                        <li><a href="RBkandangA.php">Kandang A</a></li>
-                        <li><a href="RBkandangB.php">Kandang B</a></li>
-                        <li><a href="RBkandangC.php">Kandang C</a></li>
-                        <li><a href="RBkandangD.php">kandang D</a></li>
-                    </ul>
-				</li>
+              
                 <!-- <li class="nav-item"><a href="Logout.php" class="nav-link "><span class="pcoded-micon"><i class="fas fa-fw fa-sign-out-alt"></i></span><span class="pcoded-mtext">Logout</span></a></li> -->
                 
             </ul>
